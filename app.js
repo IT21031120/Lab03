28f04d846c1709686e2f3c3ec8e58eef6dc490e3
IT21031120 <it21031120@my.sliit.lk>
2023-03-04 10:42:02 +0530

    CreatingWebServer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 //Read File
 const fs = require('fs');
+const { request } = require('http');
+const { type } = require('os');
 fs.readFile('file.txt', 'utf8', function (err, data) {
     if (err) throw err;
     console.log(data);
@@ -13,3 +15,11 @@ fs.writeFile('file.txt', "Hello world!!", function (err) {
     if (err) throw err;
     console.log("File Saved");
 })
+
+//Creating A Web Server
+const http = request("http");
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write("Hello World!!");
+    res.end();
+}).listen(8080);
